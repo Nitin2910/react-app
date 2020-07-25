d2dd17c2744a8019da4c6c5e7b828dfb3e6f0a0f
nitinjohri29 <46794756+nitinjohri29@users.noreply.github.com>
2020-07-25 21:13:28 +0530

    react

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import "./App.css";
+import Person from "./Person/Person";
+
+class App extends Component {
+  state = {
+    persons: [
+      { name: "Stoks", age: 28 },
+      { name: "Buttler", age: 31 },
+      { name: "Root", age: 32 }
+    ]
+  };
+
+  switchNameHandler = () => {
+    this.setState({
+      persons: [
+        { name: "Holder", age: 28 },
+        { name: "Brooks", age: 31 },
+        { name: "Campbell", age: 32 }
+      ]
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi React</h1>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        >
+          My Hobby : TimePass
+        </Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
+      </div>
+    );
+    // return React.createElement('div',null,'h1','Hi React!!');
+    //return React.createElement('div',null,React.createElement('h1',null,'Hi React!!!'));
+    //return React.createElement('div',{className : 'App'},React.createElement('h1',null,'Hi React!!!'));
+  }
+}
+
+export default App;
