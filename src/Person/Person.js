ac7738c76b728f2510408fb81fae4475f89eceac
nitinjohri29 <46794756+nitinjohri29@users.noreply.github.com>
2020-07-25 20:48:14 +0530

    new featue

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -4,7 +4,7 @@ const person = props => {
   return (
     <div>
       <p>
-        My name is {props.name} and I am My age is {props.age} years old!!
+        My name is a {props.name} and I am My age is {props.age} years old!!
       </p>
       <p>{props.children}</p>
     </div>
